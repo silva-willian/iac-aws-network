5b24580329fb79147462ba74612e695bee77c67b
Willian da Silva <silva.willian@outlook.com>
2021-07-29 21:25:46 -0300

    Criando as variaveis

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "product" {
+  default = "rocketseat"
+}
+
+variable "owner" {
+  default = "squad-rocketseat"
+}
+
+variable "createdBy" {
+  default = "devops-tools"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "envVersion" {
+  default = "v1"
+}
